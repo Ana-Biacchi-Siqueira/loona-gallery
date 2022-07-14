aec8b93452ab4315ac1b22d316115a8af0499682
Ana Siqueira <ana.siqueira@gpnet.com.br>
2022-07-14 15:29:16 -0300

    Finished chapter 2 of the Angular course foundations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,5 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
-export class AppComponent {}
+  })
+
+  export class AppComponent {
+
+  photos = [
+    {
+      url:"https://pbs.twimg.com/media/D693yHdW0AAQnZ6?format=jpg&name=4096x4096",
+      description:"A photo of one of the Loona members called Gowon, she's holding her hands in an X format over her mouth, she is also blonde in this photo taken by pink skies on twitter."},
+    {
+      url:"https://pbs.twimg.com/media/ERZSb5LW4AAOpD5.jpg",
+      description:"A photo of Jeon Heejin, one of the members of Loona, she's wearing a necklace chain across the middle of her face while looking into the camera that was shot by Liptight on twitter."
+    }];
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { PhotoComponent } from './photo/photo.component';
+import { PhotoComponent } from './photos/photo/photo.component';
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
