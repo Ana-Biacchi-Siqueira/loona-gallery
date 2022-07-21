c7a0585606b32c69affa851a13653af1d9d0f701
Ana Siqueira <ana.siqueira@gpnet.com.br>
2022-07-21 17:15:44 -0300

    Finalized sixth chapter

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -7,9 +7,10 @@ import { PhotoComponent } from './photo/photo.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
 import { FilterByDescription } from './photo-list/photos/photos-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 @NgModule({
-  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescription ],
+  declarations: [ PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescription, LoadButtonComponent ],
   imports: [ HttpClientModule, CommonModule ]
 })
 
